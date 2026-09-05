4d8d2bfcd8a15e2e48bacde76316896c45780f16
Gerardo Llamas <tu-correo@ejemplo.com>
2026-09-05 02:28:41 -0600

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PokedexComponent } from './components/pokedex/pokedex';
+import { FavoritosComponent } from './components/favoritos/favoritos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: PokedexComponent },
+    { path: 'favoritos', component: FavoritosComponent }
+];

a36069d687f8db7591b09fe448be9f1832ce1fd8
Gerardo Llamas <tu-correo@ejemplo.com>
2026-09-05 16:54:19 -0600

    ruta de vista detalles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { PokedexComponent } from './components/pokedex/pokedex';
 import { FavoritosComponent } from './components/favoritos/favoritos';
+import { DetallesComponent } from './components/detalles pokemon/detalles';
 
 export const routes: Routes = [
     { path: '', component: PokedexComponent },
-    { path: 'favoritos', component: FavoritosComponent }
+    { path: 'favoritos', component: FavoritosComponent },
+    { path: 'detalles/:id', component: DetallesComponent }
 ];

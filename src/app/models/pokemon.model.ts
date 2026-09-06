b45f4c785747e3c87ba6abfa3c4a74ca8bec132f
Gerardo Llamas <tu-correo@ejemplo.com>
2026-09-06 02:21:08 -0600

    actualizacion del modelo para vista detalle

--- src/app/models/pokemon.model.ts
+++ src/app/models/pokemon.model.ts
@@ -5,6 +5,29 @@ export interface Pokemon {
     types: string[];
 }
 
+export interface PokemonAbility {
+  name: string;
+  isHidden: boolean;
+}
+
+export interface PokemonStat {
+  name: string;
+  baseStat: number;
+}
+
+export interface PokemonDetail {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  types: string[];
+  abilities: PokemonAbility[];
+  description: string;
+  stats: PokemonStat[];
+  image: string;
+  artwork: string;
+}
+
 export interface PokemonListResponse {
   count: number;
   next: string | null;

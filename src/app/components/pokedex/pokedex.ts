90c8362ad6b9a24fc359eb004d9c3f95c0c98d4e
Gerardo Llamas <tu-correo@ejemplo.com>
2026-09-05 22:45:07 -0600

    conectar services de pokemon a pantalla principal (remplazando arreglos de prueba por datos de la api)

--- src/app/components/pokedex/pokedex.ts
+++ src/app/components/pokedex/pokedex.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { PokemonService } from '../../services/pokemon.service';
 
 interface Pokemon {
     id: number;
     name: string;
-    type: string;
+    image: string;
+    types: string[];
 }
 
 @Component({
@@ -16,62 +18,53 @@ interface Pokemon {
     templateUrl: './pokedex.html',
     styleUrl: './pokedex.css'
 })
+
 export class PokedexComponent{
     searchTerm = '';
     selectedType = '';
     currentPage = 1;
     pageSize = 20;
 
-    pokemon: Pokemon[] = [
-      { id: 1, name: 'Bulbasaur', type: 'Grass' },
-      { id: 2, name: 'Ivysaur', type: 'Grass' },
-      { id: 3, name: 'Venusaur', type: 'Grass' },
-      { id: 4, name: 'Charmander', type: 'Fire' },
-      { id: 5, name: 'Charmeleon', type: 'Fire' },
-      { id: 6, name: 'Charizard', type: 'Fire' },
-      { id: 7, name: 'Squirtle', type: 'Water' },
-      { id: 8, name: 'Wartortle', type: 'Water' },
-      { id: 9, name: 'Blastoise', type: 'Water' },
-      { id: 10, name: 'Caterpie', type: 'Bug' },
-      { id: 11, name: 'Metapod', type: 'Bug' },
-      { id: 12, name: 'Butterfree', type: 'Bug' },
-      { id: 13, name: 'Weedle', type: 'Poison' },
-      { id: 14, name: 'Kakuna', type: 'Poison' },
-      { id: 15, name: 'Beedrill', type: 'Poison' },
-      { id: 16, name: 'Pidgey', type: 'Flying' },
-      { id: 17, name: 'Pidgeotto', type: 'Flying' },
-      { id: 18, name: 'Pidgeot', type: 'Flying' },
-      { id: 19, name: 'Rattata', type: 'Normal' },
-      { id: 20, name: 'Raticate', type: 'Normal' },
-      { id: 21, name: 'Spearow', type: 'Flying' },
-      { id: 22, name: 'Fearow', type: 'Flying' },
-      { id: 23, name: 'Ekans', type: 'Poison' },
-      { id: 24, name: 'Arbok', type: 'Poison' },
-      { id: 25, name: 'Pikachu', type: 'Electric' },
-      { id: 26, name: 'Raichu', type: 'Electric' },
-      { id: 27, name: 'Sandshrew', type: 'Ground' },
-      { id: 28, name: 'Sandslash', type: 'Ground' },
-      { id: 29, name: 'Nidoran', type: 'Poison' },
-      { id: 30, name: 'Nidorina', type: 'Poison' }
-    ];
 
+    constructor(private pokemonService: PokemonService) {
+      this.loadPokemons();
+    }
+
+    pokemon: Pokemon[] = [];
+
+    loadPokemons(): void {
+    this.pokemonService.getPokemons(20, 0).subscribe({
+      next: (pokemon) => {
+        this.pokemon = pokemon;
+      },
+      error: (error) => {
+        console.error('Error al cargar los Pokémon:', error);
+      }
+    });
+    }
+
+    //obtener tipos unicos de los pokemones
   get types(): string[] {
-    return [...new Set(this.pokemon.map(p => p.type))];
+    return [...new Set(this.pokemon.flatMap((pokemon) => pokemon.types)
+    )];
   }
 
+  // Filtrar los pokemones por nombre, numero o tipo
   get filteredPokemon(): Pokemon[] {
-    const search = this.searchTerm.toLowerCase();
+  const search = this.searchTerm.trim().toLowerCase();
 
-    return this.pokemon.filter((poke) => {
-        const matchesName = poke.name.toLowerCase().includes(search);
-        const matchesNumber = poke.id.toString().includes(this.searchTerm);
-        const formattedNumber = poke.id.toString().padStart(3, '0');
+  return this.pokemon.filter((poke) => {
+    const matchesName = poke.name.toLowerCase().includes(search);
+    const matchesNumber =
+      poke.id.toString().includes(search) ||
+      poke.id.toString().padStart(3, '0').includes(search);
 
-        const matchesType = this.selectedType ? poke.type === this.selectedType : true;
-        const pokeNumber = matchesNumber || formattedNumber.includes(this.searchTerm);
-        return (matchesName || pokeNumber) && matchesType;
-    });
+    const matchesType = this.selectedType
+      ? poke.types.includes(this.selectedType)
+      : true;
 
+    return (matchesName || matchesNumber) && matchesType;
+  });
   }
 
   get paginatedPokemon(): Pokemon[] {
@@ -103,4 +96,8 @@ export class PokedexComponent{
         this.currentPage++;
     }
   }
+
+ 
 }
+
+

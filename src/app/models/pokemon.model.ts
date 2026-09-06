370c12c648349f471309a0dd744b918f90e0c76b
Gerardo Llamas <tu-correo@ejemplo.com>
2026-09-06 14:04:05 -0600

    modelo  de campos  de pokemopage

--- src/app/models/pokemon.model.ts
+++ src/app/models/pokemon.model.ts
@@ -1,3 +1,10 @@
+export interface PokemonPage{
+    count: number;
+    next: string | null;
+    previous: string | null;
+    results: Pokemon[];
+}
+
 export interface Pokemon {
     id: number;
     name: string;

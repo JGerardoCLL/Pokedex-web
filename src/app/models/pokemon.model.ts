0fe04b45f28badd92fef9d4bd4d1de203a72b7de
Gerardo Llamas <tu-correo@ejemplo.com>
2026-09-07 12:59:39 -0600

    interfaces en modelo

--- src/app/models/pokemon.model.ts
+++ src/app/models/pokemon.model.ts
@@ -39,8 +39,59 @@ export interface PokemonListResponse {
   count: number;
   next: string | null;
   previous: string | null;
-  results: {
-    name: string;
-    url: string;
+  results: PokemonListItem[];
+}
+
+export interface PokemonListItem {
+  name: string;
+  url: string;
+}
+
+export interface PokemonTypeResponse {
+  pokemon: {
+    pokemon: PokemonListItem;
+  }[];
+}
+
+export interface PokemonApiResponse {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  types: {
+    type: {
+      name: string;
+    };
+  }[];
+  abilities: {
+    ability: {
+      name: string;
+    };
+    is_hidden: boolean;
+  }[];
+  stats: {
+    base_stat: number;
+    stat: {
+      name: string;
+    };
+  }[];
+  sprites: PokemonSprites;
+}
+
+export interface PokemonSprites {
+  front_default: string | null;
+  other: {
+    'official-artwork': {
+      front_default: string | null;
+    };
+  };
+}
+
+export interface PokemonSpeciesResponse {
+  flavor_text_entries: {
+    flavor_text: string;
+    language: {
+      name: string;
+    };
   }[];
 }

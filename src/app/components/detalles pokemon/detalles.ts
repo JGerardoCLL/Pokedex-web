4f941de9335c94fc8c144fd292c846032d256fe3
Gerardo Llamas <tu-correo@ejemplo.com>
2026-09-06 20:14:18 -0600

    importar servicio de favoritos y metodo para estado de favorito

--- src/app/components/detalles pokemon/detalles.ts
+++ src/app/components/detalles pokemon/detalles.ts
@@ -4,6 +4,8 @@ import { CommonModule } from '@angular/common';
 import { Observable, switchMap } from 'rxjs';
 import { PokemonService } from '../../services/pokemon.service';
 import { PokemonDetail } from '../../models/pokemon.model';
+import { FavoritesService } from '../../services/favorites.service';
+
 
 @Component({
     selector: 'app-detalles',
@@ -18,6 +20,7 @@ export class DetallesComponent {
 
     constructor(
       private route: ActivatedRoute,
+      private favoritesService: FavoritesService,
       private pokemonService: PokemonService
     ) {
       this.pokemon$ = this.route.paramMap.pipe(
@@ -30,6 +33,17 @@ export class DetallesComponent {
       this.route.queryParamMap.subscribe((params) => {
         this.returnPage = Number(params.get('page')) || 1;
       });
+    }
+
+    isFavorite(id: number): boolean {
+    return this.favoritesService.isFavorite(id);
+    }
+    // Método para alternar el estado de favorito de un Pokémon
+    toggleFavorite(id: number, event: Event): void {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.favoritesService.toggle(id);
     }
 
 

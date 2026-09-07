2eb9990d3b1b08a9bd381e9fe70ec4262d3daf88
Gerardo Llamas <tu-correo@ejemplo.com>
2026-09-06 23:45:36 -0600

    ruta comodin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { PokedexComponent } from './components/pokedex/pokedex';
 import { FavoritosComponent } from './components/favoritos/favoritos';
 import { DetallesComponent } from './components/detalles pokemon/detalles';
+import { EstadosComponent } from './components/estados de interfaz/estados';
 
 export const routes: Routes = [
     { path: '', component: PokedexComponent },
     { path: 'favoritos', component: FavoritosComponent },
-    { path: 'pokemon/:id', component: DetallesComponent }
+    { path: 'pokemon/:id', component: DetallesComponent },
+    { path: '**', component: EstadosComponent }
 ];
